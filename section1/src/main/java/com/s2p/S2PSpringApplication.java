6dcc748a97340cd698ec2f824a3d43cabedca7e3
root <pranayramteke613@gmail.com>
2025-08-05 14:06:47 +0530

    Spring Boot Hello World Web Service

---
+++ section1/src/main/java/com/s2p/S2PSpringApplication.java
@@ -0,0 +1,13 @@
+package com.s2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2PSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2PSpringApplication.class, args);
+	}
+
+}
