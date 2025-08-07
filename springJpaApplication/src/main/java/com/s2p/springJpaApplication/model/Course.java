c4425f1a3f0ac3f1604ecff18fa1fe1ee1500366
root <pranayramteke613@gmail.com>
2025-08-07 14:45:28 +0530

    Added Code For JPA Repository

---
+++ springJpaApplication/src/main/java/com/s2p/springJpaApplication/model/Course.java
@@ -0,0 +1,9 @@
+package com.s2p.springJpaApplication.model;
+
+public class Course
+{
+    private Long courseId;
+    private String courseName;
+    private Byte duration;
+    private String description;
+}
