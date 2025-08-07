28b11dcb2c6b63d611e7fc9c96b163b8c60ff2b6
root <pranayramteke613@gmail.com>
2025-08-07 14:45:28 +0530

    Added Code For JPA Repository

--- springJpaApplication/src/main/java/com/s2p/springJpaApplication/controller/StudentController.java
+++ springJpaApplication/src/main/java/com/s2p/springJpaApplication/controller/StudentController.java
@@ -1,12 +1,43 @@
 package com.s2p.springJpaApplication.controller;
 
+import com.s2p.springJpaApplication.model.Student;
 import com.s2p.springJpaApplication.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController
 {
     @Autowired
     StudentService studentService;
+
+    // POST http://localhost:8080/api/v1/student
+    @PostMapping("/api/v1/student")
+    public Student createStudentRecord(@RequestBody Student student)
+    {
+        Student createStudent = studentService.saveStudent(student);
+        return createStudent;
+    }
+
+    @GetMapping("/api/v1/student")
+    public List<Student> getAllStudents()
+    {
+       List<Student> studentList =   studentService.fetchAllStudents();
+       return studentList;
+    }
+
+    // http://localhost:8080/api/v1/student/1
+    @DeleteMapping("/api/v1/student/{id}")
+    public String removeStudentById(@PathVariable("id")Long id)
+    {
+        boolean result = studentService.deleteStudentById(id);
+        String message = result == true ? "Student Has Been Deleted." : "Unable To Delete Student.";
+        return message;
+    }
+
+    
+
+
 }
--- springJpaApplication/src/main/java/com/s2p/springJpaApplication/service/StudentService.java
+++ springJpaApplication/src/main/java/com/s2p/springJpaApplication/service/StudentService.java
@@ -1,12 +1,48 @@
 package com.s2p.springJpaApplication.service;
 
+import com.s2p.springJpaApplication.model.Student;
 import com.s2p.springJpaApplication.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService
 {
     @Autowired
     StudentRepository studentRepository;
+
+    public Student saveStudent(Student student)
+    {
+        Student savedStudent = studentRepository.save(student);
+        return savedStudent;
+    }
+
+    public List<Student> fetchAllStudents()
+    {
+        // SELECT * FROM student;
+        List<Student> studentList = studentRepository.findAll();
+        return studentList;
+    }
+
+
+
+    public Boolean deleteStudentById(Long studentId)
+    {
+        boolean isDeleted = false;
+
+        Optional<Student> studentOptional =  studentRepository.findById(studentId);
+
+        if(studentOptional.isPresent())
+        {
+            isDeleted = true;
+            Student student = studentOptional.get();
+            studentRepository.delete(student);
+        }
+
+        return isDeleted;
+    }
+
 }
